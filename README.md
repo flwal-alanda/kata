6c46a3aa8037c32f37715822c14c0154fd1beb9f
Florian Waltersdorfer <florian.waltersdorfer@alanada.io>
2020-10-02 16:21:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Start with [Word Count I](https://ccd-school.de/coding-dojo/agility-katas/word-count-i/), then look under [Agility Katas - Word Count](https://ccd-school.de/coding-dojo/#cd8) for more exercises.
---
+++ src/test/java/io/alanda/AppTest.java
@@ -0,0 +1,13 @@
+package io.alanda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AppTest {
+
+    @Test
+    public void shouldFail() {
+        fail("go add some code to test");
+    }
+}
